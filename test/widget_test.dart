190cd6ac2828d52a5d0ef704d37ab7a500da10e4
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-02 00:39:06 +0200

    update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:moscore/app/app.dart';
 
-import 'package:moscore/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

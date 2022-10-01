edf78a8032afeda007a54ec454e750924829db33
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-02 00:36:05 +0200

    update assets.dart

--- lib/presentation/resources/assets/assets.dart
+++ lib/presentation/resources/assets/assets.dart
@@ -3,4 +3,8 @@ const String jsonPath = 'assets/json';
 
 class AssetsResources {}
 
-class JsonResources {}
+class JsonResources {
+  static const String goal = '$jsonPath/goal.json';
+  static const String lineUp = '$jsonPath/lineup.json';
+  static const String player = '$jsonPath/player.json';
+}

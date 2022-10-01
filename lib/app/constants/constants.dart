0634f88e02f6a4f4e4695c46b33054e3bd61dc29
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-02 00:09:09 +0200

    update constants.dart

---
+++ lib/app/constants/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String baseUrl = 'https://api-football-v1.p.rapidapi.com/v3';
+  static const String countries = '/countries';
+  static const String teamStatistics = '/teams/statistics';
+  static const String teamsInfo = '/teams';
+}

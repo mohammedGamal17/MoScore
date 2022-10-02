a7377a850324dccaf6467adfa4ccdf885b04d090
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-02 19:05:17 +0200

    update app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../presentation/resources/routes/routes_manager.dart';
-import '../presentation/view/on_boarding_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});

a9680531ca70294057a99e2ab8c8c0129e438ec2
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-02 02:02:58 +0200

    update on_boarding.dart

--- lib/presentation/view/on_boarding.dart
+++ lib/presentation/view/on_boarding.dart
@@ -5,24 +5,11 @@ import 'package:moscore/presentation/resources/colors/color_manager.dart';
 import 'package:moscore/presentation/resources/string/string_manager.dart';
 import 'package:moscore/presentation/resources/values/values_manager.dart';
 
-import '../../domain/entities/entities.dart';
+
 import '../resources/assets/assets.dart';
 
 class OnBoarding extends StatelessWidget {
   OnBoarding({super.key});
-
-  // final List<OnBoardModel> _list = [
-  //   OnBoardModel(StringManager.onBoardModelTitle1,
-  //       StringManager.onBoardModelSubTitle1, Lottie.asset(JsonResources.goal)),
-  //   OnBoardModel(
-  //       StringManager.onBoardModelTitle2,
-  //       StringManager.onBoardModelSubTitle1,
-  //       Lottie.asset(JsonResources.lineUp)),
-  //   OnBoardModel(
-  //       StringManager.onBoardModelTitle3,
-  //       StringManager.onBoardModelSubTitle1,
-  //       Lottie.asset(JsonResources.player)),
-  // ];
 
   final List<PageViewModel> _list = [
     PageViewModel(
@@ -36,10 +23,9 @@ class OnBoarding extends StatelessWidget {
       image: Lottie.asset(JsonResources.lineUp),
     ),
     PageViewModel(
-      body: StringManager.onBoardModelTitle3,
-      title: StringManager.onBoardModelSubTitle1,
+      body: StringManager.onBoardModelTitle2,
+      title: StringManager.onBoardModelSubTitle2,
       image: Lottie.asset(JsonResources.player),
-      reverse: true,
     ),
   ];
 
@@ -47,14 +33,25 @@ class OnBoarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(AppSize.s20),
-          child: IntroductionScreen(
-            pages: _list,
-            curve: Curves.bounceInOut,
-            
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(AppSize.s20),
+        child: IntroductionScreen(
+          pages: _list,
+          curve: Curves.bounceInOut,
+          onDone: () => null,
+          onSkip: () => null,
+          // You can override onSkip callback
+          showSkipButton: true,
+          skipOrBackFlex: 0,
+          nextFlex: 0,
+          showBackButton: false,
+          //rtl: true, // Display as right-to-left
+          back: const Icon(Icons.arrow_back),
+          skip: const Text(StringManager.skip,
+              style: TextStyle(fontWeight: FontWeight.w600)),
+          next: const Icon(Icons.arrow_forward),
+          done: const Text(StringManager.done,
+              style: TextStyle(fontWeight: FontWeight.w600)),
         ),
       ),
     );

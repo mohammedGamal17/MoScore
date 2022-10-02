8fb5ce0e32e6a1765f80bfd1ce09471b7b09680b
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-02 02:03:07 +0200

    update entities.dart

--- lib/domain/entities/entities.dart
+++ lib/domain/entities/entities.dart
@@ -1,11 +1,3 @@
-class OnBoardModel {
-  String title;
-  String subTitle;
-  String image;
-
-  OnBoardModel(this.title, this.subTitle, this.image);
-}
-
 class Countries {
   int results;
   List<Response> response;

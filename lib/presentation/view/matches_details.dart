f9829c8613829d66ed1a659df8812da34da10f94
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-02 19:03:55 +0200

    update matches_details.dart

---
+++ lib/presentation/view/matches_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MatchesDetailsView extends StatelessWidget {
+  const MatchesDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

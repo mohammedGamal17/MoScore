ba6af0ef6ce498bd46ba5aa509822d0ae7663bb6
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-02 19:03:10 +0200

    add GeneratedPluginRegistrant.swift , GoogleService-Info.plist , firebase_app_id_file.json

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus_macos
+import firebase_core
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }

1bcb8838a5b25b86be12724c8b5234445f2bd4db
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-03 03:33:16 +0200

    add fonts_manager.dart

---
+++ lib/presentation/resources/fonts/fonts_manager.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class FontsConstants {
+  static const String fontFamily = 'TitilliumWeb';
+}
+
+class FontsWight {
+  static const FontWeight extraLight = FontWeight.w200;
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight black = FontWeight.w900;
+}
+
+class FontsSize {
+  static const double s6 = 6.0;
+  static const double s8 = 8.0;
+  static const double s10 = 10.0;
+  static const double s11 = 11.0;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s17 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s22 = 22.0;
+  static const double s24 = 24.0;
+  static const double s26 = 26.0;
+  static const double s28 = 28.0;
+}

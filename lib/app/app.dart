942a0915ffd53467e37dfb1c87ff27f334188359
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-03 09:21:33 +0200

    update app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moscore/presentation/resources/theme/theme_manager.dart';
 
 import '../presentation/resources/routes/routes_manager.dart';
+import '../presentation/view_model/cubit/auth_cubit/auth_cubit.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: RoutesGenerator.getRoute,
-      initialRoute: Routes.splash,
-      theme: getLightModeThemeData(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AuthCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: RoutesGenerator.getRoute,
+        initialRoute: Routes.splash,
+        theme: getLightModeThemeData(),
+      ),
     );
   }
 }

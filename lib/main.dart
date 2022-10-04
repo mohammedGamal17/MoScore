390e46d22205bee00a9205ad1aad24ddc4385a23
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-04 13:10:59 +0200

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:moscore/presentation/view_model/cubit/app_bloc_observer.dart';
 
 import 'app/app.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = AppBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );

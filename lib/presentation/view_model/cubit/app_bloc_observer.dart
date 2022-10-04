2283101f06ade655ae8023c831515a8b5f6197b8
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-10-04 13:10:51 +0200

    update app_bloc_observer.dart

---
+++ lib/presentation/view_model/cubit/app_bloc_observer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/foundation.dart';
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    if (bloc is Cubit) {
+      if (kDebugMode) {
+        print(change);
+      }
+    }
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    if (kDebugMode) {
+      print(transition);
+    }
+  }
+}

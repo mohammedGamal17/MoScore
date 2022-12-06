f094671154c7662c148f82c3eeac2c94483df5a2
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-12-06 14:40:23 +0200

    update auth_cubit.dart

--- lib/presentation/view_model/cubit/auth_cubit/auth_cubit.dart
+++ lib/presentation/view_model/cubit/auth_cubit/auth_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:moscore/domain/use_cases/auth_use_cases/register_use_case.dart';
 
 import 'auth_state.dart';

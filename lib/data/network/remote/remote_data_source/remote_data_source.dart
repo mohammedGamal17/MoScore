e7368cf85c65e19bc843cb08d0b179d2b30c6781
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-12-08 06:50:09 +0200

    update

---
+++ lib/data/network/remote/remote_data_source/remote_data_source.dart
@@ -0,0 +1,5 @@
+abstract class BaseRemoteDataSource {}
+
+class RemoteDataSourceImplement extends BaseRemoteDataSource {
+  RemoteDataSourceImplement();
+}
--- lib/data/repositories_implementation/repositories_implemntation.dart
+++ lib/data/repositories_implementation/repositories_implemntation.dart
@@ -1,6 +1,6 @@
-import 'package:dartz/dartz.dart';
-import 'package:moscore/app/failure/Failure.dart';
-import 'package:moscore/domain/entities/entities.dart';
-import 'package:moscore/domain/repositories/repositories.dart';
+import '../../domain/repositories/repositories.dart';
 
-class RepositoriesImplementation extends Repositories {}
+class RepositoriesImplementation implements Repositories {
+  RepositoriesImplementation();
+}
+

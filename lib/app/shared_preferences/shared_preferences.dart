cef54b4c03107f148bf0c38f5f6b68a538494c79
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-12-20 11:44:43 +0200

    update shared_preferences.dart

--- lib/app/shared_preferences/shared_preferences.dart
+++ lib/app/shared_preferences/shared_preferences.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 const String isOnBoardingShow = 'isOnBoardingShow';
 const String isSignIn = 'isLoggedIn';
 const String uId = 'uId';
+const String isAnonymously = 'isAnonymously';
 
 class AppPreferences {
   final SharedPreferences _sharedPreferences;
@@ -41,5 +42,24 @@ class AppPreferences {
   /// Get Value Of UID
   String? getUId() {
     return _sharedPreferences.getString(uId);
+  }
+
+  /// Set if SignIn or Anonymously
+  setIsAnonymously({
+    required bool isAnonymous,
+  }) {
+    _sharedPreferences.setBool(isAnonymously, isAnonymous);
+  }
+
+  /// Get if SignIn or Anonymously
+  bool getIsAnonymously() {
+    return _sharedPreferences.getBool(isAnonymously) ?? false;
+  }
+
+  /// Clear All Cache
+  clearAllCache() async {
+   await _sharedPreferences.remove(isSignIn);
+   await _sharedPreferences.remove(uId);
+   await _sharedPreferences.remove(isAnonymously);
   }
 }

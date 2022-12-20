4d3023319626df8128f1a8425969da259f9eccb7
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-12-20 15:05:29 +0200

    update base_use_case.dart

--- lib/domain/use_cases/base_use_case.dart
+++ lib/domain/use_cases/base_use_case.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:moscore/app/failure/Failure.dart';
 
-abstract class BaseUseCases<Output, Inputs> {
+abstract class BaseUseCase<Output, Inputs> {
   Future<Either<Failure, Output>> call(Inputs inputs);
 }

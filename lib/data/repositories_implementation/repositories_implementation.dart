96a1603170ce643f7eea5f723af766bfee571477
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-12-20 17:54:27 +0200

    Update

--- lib/data/repositories_implementation/repositories_implementation.dart
+++ lib/data/repositories_implementation/repositories_implementation.dart
@@ -1,6 +1,15 @@
+import 'package:dartz/dartz.dart';
+
+import '../../app/failure/Failure.dart';
+import '../../domain/entities/entities.dart';
 import '../../domain/repositories/repositories.dart';
 
 class RepositoriesImplementation implements Repositories {
   RepositoriesImplementation();
-}
 
+  @override
+  Future<Either<Failure, List<FixtureResponse>>> getLiveFixture() {
+    // TODO: implement getLiveFixture
+    throw UnimplementedError();
+  }
+}

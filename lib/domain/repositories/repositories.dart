fdff2debf55cda005b7e96c2bc20630cfa7c3792
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-12-20 15:05:59 +0200

    Update repositories.dart

--- lib/domain/repositories/repositories.dart
+++ lib/domain/repositories/repositories.dart
@@ -1,1 +1,8 @@
-abstract class Repositories {}
+import 'package:dartz/dartz.dart';
+
+import '../../app/failure/Failure.dart';
+import '../entities/entities.dart';
+
+abstract class Repositories {
+  Future<Either<Failure,List<FixtureResponse>>> getLiveFixture();
+}

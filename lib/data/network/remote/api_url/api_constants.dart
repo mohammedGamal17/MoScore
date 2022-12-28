cc83efcf6d3c6d064a77753e184c82a5feaa8efc
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-12-28 21:31:07 +0200

    Update api_constants.dart

--- lib/data/network/remote/api_url/api_constants.dart
+++ lib/data/network/remote/api_url/api_constants.dart
@@ -9,14 +9,14 @@ class APIConstants {
   static const String _token1 = 'ed307c6f236bd2c88d2c2f6b325281c8';
 
   static const Map<String, dynamic> header = {
-    'X-RapidAPI-Key': _token1,
+    'X-RapidAPI-Key': _token,
   };
   static const String baseUrl = 'https://api-football-v1.p.rapidapi.com/v3';
   static const String baseUrl1 = 'https://v3.football.api-sports.io';
-  static const String fixturesLive = '$baseUrl1/fixtures?live=all';
+  static const String fixturesLive = '$baseUrl/fixtures?live=all';
 
   static String getFixturesByID({required int id}) =>
-      '$baseUrl1/fixtures?id=$id';
+      '$baseUrl/fixtures?id=$id';
 
-  static String getTodayMatch = '$baseUrl1/fixtures?date=$formattedDate';
+  static String getTodayMatch = '$baseUrl/fixtures?date=$formattedDate';
 }

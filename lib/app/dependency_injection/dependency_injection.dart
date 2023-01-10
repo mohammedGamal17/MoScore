422c2000b69cacd08ab1919b89f2253be997fce2
mohammedGamal7 <mohammed_gamal7@outlook.com>
2023-01-10 17:03:25 +0200

    Update DI

--- lib/app/dependency_injection/dependency_injection.dart
+++ lib/app/dependency_injection/dependency_injection.dart
@@ -8,13 +8,15 @@ import '../../data/network/remote/remote_data_source/remote_data_source.dart';
 import '../../data/repositories_implementation/repositories_implementation.dart';
 import '../../domain/repositories/repositories.dart';
 import '../../domain/use_cases/all_leagues_use_case.dart';
-import '../../domain/use_cases/get_standing_use_case.dart';
+import '../../domain/use_cases/standing_use_case.dart';
 import '../../domain/use_cases/live_fixture_use_case.dart';
+import '../../domain/use_cases/team_info_use_case.dart';
 import '../../domain/use_cases/today_matches_use_case.dart';
 import '../../presentation/view_model/cubit/fixture_cubit/fixture_cubit.dart';
 import '../../presentation/view_model/cubit/leagues_cubit/leagues_cubit.dart';
 import '../../presentation/view_model/cubit/profile_cubit/profile_cubit.dart';
 import '../../presentation/view_model/cubit/auth_cubit/auth_cubit.dart';
+import '../../presentation/view_model/cubit/team_cubit/team_cubit.dart';
 import '../shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
@@ -78,6 +80,12 @@ class Di {
       ),
     );
 
+    getIt.registerLazySingleton<TeamInfoUseCase>(
+      () => TeamInfoUseCase(
+        getIt<Repositories>(),
+      ),
+    );
+
     // Cubit
     getIt.registerFactory<AuthCubit>(
       () => AuthCubit(
@@ -103,6 +111,13 @@ class Di {
         getIt<NetworkInfo>(),
         getIt<AllLeaguesUseCase>(),
         getIt<GetStandingUseCase>(),
+      ),
+    );
+
+    getIt.registerFactory<TeamCubit>(
+      () => TeamCubit(
+        getIt<NetworkInfo>(),
+        getIt<TeamInfoUseCase>(),
       ),
     );
   }

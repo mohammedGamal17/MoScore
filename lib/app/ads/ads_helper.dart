00101628434d13d9835d3eed35751ac6691df4b3
mohammedGamal7 <mohammed_gamal7@outlook.com>
2023-01-16 04:40:20 +0200

    Update ads_helper.dart

--- lib/app/ads/ads_helper.dart
+++ lib/app/ads/ads_helper.dart
@@ -1,31 +1,89 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import '../../presentation/resources/colors/color_manager.dart';
+import '../../presentation/resources/values/values_manager.dart';
+
 class AdsHelper {
   static const bool _isTestMode = true;
 
-  static String get appOpenAdsUnitId {
-    if (_isTestMode) {
-      return 'ca-app-pub-3940256099942544/3419835294';
+  static String get appId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3641364574605890~4538952172';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3641364574605890~5931176469';
+    } else {
+      throw UnsupportedError('*** Unsupported Error OS ***');
     }
-    return 'REAL ADS UNIT';
   }
 
-  static String get bannerAdsUnitId {
+  static String get bannerUnitId {
     if (_isTestMode) {
       return 'ca-app-pub-3940256099942544/6300978111';
+    } else if (Platform.isAndroid) {
+      return 'ca-app-pub-3641364574605890/8557339800';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3641364574605890/8365768115';
+    } else {
+      throw UnsupportedError('*** Unsupported Error OS ***');
     }
-    return 'REAL ADS UNIT';
   }
 
-  static String get interstitialAdsUnits {
-    if (_isTestMode) {
-      return 'ca-app-pub-3940256099942544/1033173712';
-    }
-    return 'REAL ADS UNIT';
+  static final BannerAdListener bannerAdListener = BannerAdListener(
+    onAdLoaded: (ad) => log('Banner Loaded'),
+    onAdClicked: (ad) => log('Banner Clicked'),
+    onAdOpened: (ad) => log('Banner Opened'),
+    onAdClosed: (ad) => log('Banner Closed'),
+    onAdFailedToLoad: (ad, error) {
+      ad.dispose();
+      log('Failed to Load: $error');
+    },
+  );
+}
+
+class BannerAdComponent extends StatefulWidget {
+  const BannerAdComponent({super.key});
+
+  @override
+  State<BannerAdComponent> createState() => _BannerAdComponentState();
+}
+
+class _BannerAdComponentState extends State<BannerAdComponent> {
+  BannerAd? bannerAd;
+  final AdSize adSize = AdSize(
+    width: AppSize.s300.toInt(),
+    height: AppSize.s50.toInt(),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _bannerAdCreate();
   }
 
-  static String get interstitialVideoAdsUnits {
-    if (_isTestMode) {
-      return 'ca-app-pub-3940256099942544/8691691433';
-    }
-    return 'REAL ADS UNIT';
+  void _bannerAdCreate() {
+    bannerAd = BannerAd(
+      size: adSize,
+      adUnitId: AdsHelper.bannerUnitId,
+      listener: AdsHelper.bannerAdListener,
+      request: const AdRequest(),
+    )..load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return bannerAd != null
+        ? Center(
+          child: Container(
+              color: ColorManager.primary,
+              width: adSize.width.toDouble(),
+              height: adSize.height.toDouble(),
+              child: AdWidget(ad: bannerAd!),
+            ),
+        )
+        : Container();
   }
 }
